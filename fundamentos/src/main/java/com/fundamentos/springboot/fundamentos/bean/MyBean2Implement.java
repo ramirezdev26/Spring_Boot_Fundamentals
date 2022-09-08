14bd8b0d632d26064b1b9c190f86e4174e6208a5
santiago ramirez cano <santiago.ramirezc26@gmail.com>
2022-09-08 18:48:39 -0500

    Implementation of dependencies using spring boot, manual configuration of the dependencie's implementation and implementation of a dependence inside a dependence

---
+++ fundamentos/src/main/java/com/fundamentos/springboot/fundamentos/bean/MyBean2Implement.java
@@ -0,0 +1,9 @@
+package com.fundamentos.springboot.fundamentos.bean;
+
+public class MyBean2Implement implements MyBean {
+
+    @Override
+    public void print() {
+        System.out.println("Hola desde implementacion 2 propia del bean 2");
+    }
+}
